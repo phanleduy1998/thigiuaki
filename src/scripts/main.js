dc40fc74210f83ec9d8b2afbe87ac439c9bb45c0
LÊ DUY <leduytlnnd@gmail.com>
2018-11-21 22:43:02 +0700

    aa

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+		nav: true,
+		items:1,
+		dots: false,
+		// navText: ['<i class="fa fa-arrow-left"></i>','<i class="fa fa-arrow-right"></i>'],
+		responsive:{
+			768:{
+				nav: false,
+				dots: true,
+				items:1
+			},
+		}
+	})
+	});
